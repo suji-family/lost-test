cef17eebe2f336d29efcdb9aabc0acc8bca98f5d
otw03 <ypd01476@gmail.com>
2024-06-25 20:28:22 +0900

    [feat] 사이드바 컴포넌트 추가

---
+++ src/app/_component/SideBar.tsx
@@ -0,0 +1,25 @@
+/**
+ * @description 페이지에서 공통으로 사용되는 사이드바 컴포넌트
+ */
+'use client'
+
+import Link from 'next/link'
+
+export default function SideBar() {
+  const categories = [
+    { id: 'home', path: '', label: '홈' },
+    { id: 'news', path: 'news', label: '공지사항' },
+  ]
+
+  return (
+    <nav>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <Link href={`/${category.path}`}>{category.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 
 import './reset.scss'
+import SideBar from './_component/SideBar'
 
 // 무료 폰트
 const Pretendard = localFont({
@@ -22,7 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={clsx([Pretendard.className])}>{children}</body>
+      <body className={clsx([Pretendard.className])}>
+        <div>
+          <SideBar />
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   )
 }
