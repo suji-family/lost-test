b2af96f2d032a11b712f04997106282809b48a7d
KwangEun Ahn <61385865+ahnkwang7379@users.noreply.github.com>
2024-06-26 18:01:47 +0900

    [feat] 기본 Layout에 desktop은 16px / mobile은 13px로 기본 폰트 크기 고정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+import getIsMobile from '@/lib/getIsMobile'
 import clsx from 'clsx'
 import type { Metadata } from 'next'
-
 import localFont from 'next/font/local'
-
-import './reset.scss'
 import SideBar from './_component/SideBar'
+import UserDeviceCheckProvider from './_component/UserDeviceCheckProvider'
+import styles from './layout.module.scss'
+import './reset.scss'
 
 // 무료 폰트
 const Pretendard = localFont({
@@ -21,11 +22,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const isMobile = getIsMobile()
+
   return (
     <html lang="ko">
-      <body className={clsx([Pretendard.className])}>
-        <SideBar />
-        <main>{children}</main>
+      <body
+        className={clsx([
+          Pretendard.className,
+          styles.body,
+          isMobile ? styles.mobile : styles.desktop,
+        ])}
+      >
+        <UserDeviceCheckProvider isMobile={isMobile}>
+          <SideBar />
+          <main>{children}</main>
+        </UserDeviceCheckProvider>
       </body>
     </html>
   )
