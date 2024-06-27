59c9e0c68dbe5e8cf405a1d354c485ebe20b9751
otw03 <ypd01476@gmail.com>
2024-06-27 18:54:37 +0900

    [feat] 공지사항 탭 스타일 작성

--- src/app/news/notice/_component/Tab.tsx
+++ src/app/news/notice/_component/Tab.tsx
@@ -1,22 +1,66 @@
 'use client'
 
 import Link from 'next/link'
+import clsx from 'clsx'
+import styles from './Tab.module.scss'
+import { useState } from 'react'
 
 export default function Tab() {
-  const url = '/news/notice?type='
+  const url = '/news/notice'
+  const [activeTab, setActiveTab] = useState('공지')
+  const onClickTab = (tab: string) => {
+    setActiveTab(tab)
+  }
 
   return (
     <div>
-      &nbsp; {/* note: (나중에 제거)구분을 위한 임시용 */}
-      <div>공지사항</div>
-      &nbsp;
-      <div>
-        <Link href={`${url}공지`}>공지</Link>
-        <Link href={`${url}점검`}>점검</Link>
-        <Link href={`${url}상점`}>상점</Link>
-        <Link href={`${url}이벤트`}>이벤트</Link>
+      <h2>공지사항</h2>
+      <div className={clsx([styles.tab])}>
+        <Link
+          href={{ pathname: `${url}`, query: { type: '공지' } }}
+          legacyBehavior
+        >
+          <a
+            className={clsx({ [styles.active]: activeTab === '공지' })}
+            onClick={() => onClickTab('공지')}
+          >
+            공지
+          </a>
+        </Link>
+        <Link
+          href={{ pathname: `${url}`, query: { type: '점검' } }}
+          legacyBehavior
+        >
+          <a
+            className={clsx({ [styles.active]: activeTab === '점검' })}
+            onClick={() => onClickTab('점검')}
+          >
+            점검
+          </a>
+        </Link>
+        <Link
+          href={{ pathname: `${url}`, query: { type: '상점' } }}
+          legacyBehavior
+        >
+          <a
+            className={clsx({ [styles.active]: activeTab === '상점' })}
+            onClick={() => onClickTab('상점')}
+          >
+            상점
+          </a>
+        </Link>
+        <Link
+          href={{ pathname: `${url}`, query: { type: '이벤트' } }}
+          legacyBehavior
+        >
+          <a
+            className={clsx({ [styles.active]: activeTab === '이벤트' })}
+            onClick={() => onClickTab('이벤트')}
+          >
+            이벤트
+          </a>
+        </Link>
       </div>
-      &nbsp;
     </div>
   )
 }
