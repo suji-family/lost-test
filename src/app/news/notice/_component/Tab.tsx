b2bf1d76dc7360ce061d4630f06282eecbd4679d
otw03 <ypd01476@gmail.com>
2024-06-28 15:56:11 +0900

    [style] Tab 스타일 수정

--- src/app/news/notice/_component/Tab.tsx
+++ src/app/news/notice/_component/Tab.tsx
@@ -16,29 +16,19 @@ export default function Tab({ searchParams }: Props) {
     <div>
       <h2>공지사항</h2>
       <div className={clsx([styles.tab])}>
-        <Link
-          href={{ pathname: `${url}`, query: { type: '공지' } }}
-          legacyBehavior
-        >
-          <a className={clsx({ [styles.active]: type === '공지' })}>공지</a>
+        <Link href={{ pathname: `${url}`, query: { type: '공지' } }}>
+          <div className={clsx({ [styles.active]: type === '공지' })}>공지</div>
         </Link>
-        <Link
-          href={{ pathname: `${url}`, query: { type: '점검' } }}
-          legacyBehavior
-        >
-          <a className={clsx({ [styles.active]: type === '점검' })}>점검</a>
+        <Link href={{ pathname: `${url}`, query: { type: '점검' } }}>
+          <div className={clsx({ [styles.active]: type === '점검' })}>점검</div>
         </Link>
-        <Link
-          href={{ pathname: `${url}`, query: { type: '상점' } }}
-          legacyBehavior
-        >
-          <a className={clsx({ [styles.active]: type === '상점' })}>상점</a>
+        <Link href={{ pathname: `${url}`, query: { type: '상점' } }}>
+          <div className={clsx({ [styles.active]: type === '상점' })}>상점</div>
         </Link>
-        <Link
-          href={{ pathname: `${url}`, query: { type: '이벤트' } }}
-          legacyBehavior
-        >
-          <a className={clsx({ [styles.active]: type === '이벤트' })}>이벤트</a>
+        <Link href={{ pathname: `${url}`, query: { type: '이벤트' } }}>
+          <div className={clsx({ [styles.active]: type === '이벤트' })}>
+            이벤트
+          </div>
         </Link>
       </div>
     </div>
