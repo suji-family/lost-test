839142330aaeb5a28e6e2406c28d6adb41649bcc
KwangEun Ahn <61385865+ahnkwang7379@users.noreply.github.com>
2024-06-29 17:33:18 +0900

    [feat] 로고 클릭시 홈으로 이동

--- src/app/_component/Logo.tsx
+++ src/app/_component/Logo.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import logo from './logo.png'
 
 export default function Logo() {
-  return <Image src={logo} alt="logo" height={80} />
+  return (
+    <Link href={'/'}>
+      <Image src={logo} alt="logo" height={80} />
+    </Link>
+  )
 }
