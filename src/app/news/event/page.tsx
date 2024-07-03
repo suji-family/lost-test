977703ebca2d470533cfdbb25de77feaef2f858f
otw03 <ypd01476@gmail.com>
2024-07-03 18:19:17 +0900

    [style] 이벤트 페이지 스타일 추가

--- src/app/news/event/page.tsx
+++ src/app/news/event/page.tsx
@@ -1,6 +1,7 @@
 import { EventList } from '@/model/news/events'
 import getEventList from '../_actions/getEventList'
 import Link from 'next/link'
+import styles from './page.module.scss'
 import dayjs from 'dayjs'
 
 /**
@@ -11,21 +12,22 @@ export default async function page() {
   const eventList: EventList = await getEventList()
 
   return (
-    <section>
+    <section className={styles.eventContainer}>
       {eventList &&
         eventList.map((event, index) => (
           <Link key={index} href={event.Link}>
-            <div>
+            <div className={styles.eventCard}>
               <img src={event.Thumbnail} alt={event.Title} />
               <h3>{event.Title}</h3>
-              <p>
-                이벤트 기간: {dayjs(event.StartDate).format('YYYY.MM.DD HH:mm')}{' '}
-                - {dayjs(event.EndDate).format('MM.DD HH:mm')}
-              </p>
-              <p>
-                보상 수령 기간:{' '}
+              <div>
+                <span className={styles.eventDate}>이벤트 기간</span>:{' '}
+                {dayjs(event.StartDate).format('YYYY.MM.DD HH:mm')} -{' '}
+                {dayjs(event.EndDate).format('MM.DD HH:mm')}
+              </div>
+              <div>
+                <span className={styles.rewardDate}>보상 수령 기간</span>:{' '}
                 {dayjs(event.RewardDate).format('YYYY.MM.DD HH:mm')}
-              </p>
+              </div>
             </div>
           </Link>
         ))}
