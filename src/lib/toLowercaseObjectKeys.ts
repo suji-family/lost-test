28ede72e4a067e3ca3760a28fe396224d7648496
KwangEun Ahn <61385865+ahnkwang7379@users.noreply.github.com>
2024-07-06 15:01:23 +0900

    [docs] 주석 추가

--- src/lib/toLowercaseObjectKeys.ts
+++ src/lib/toLowercaseObjectKeys.ts
@@ -2,6 +2,10 @@ const toLowercaseFirstChar = (str: string): string => {
   return str.charAt(0).toLowerCase() + str.slice(1)
 }
 
+/**
+ * @description
+ * 배열 내 오브젝트의 키 값의 첫 글자를 소문자로 바꿔주는 유틸리티 함수
+ */
 const transformKeys = <T extends Record<string, any>>(
   obj: T,
 ): { [K in keyof T as K extends string ? Lowercase<K> : never]: T[K] } => {
