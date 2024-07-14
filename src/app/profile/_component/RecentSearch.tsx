5596df8ae715b71e424e072623a8a3490d368ab8
otw03 <ypd01476@gmail.com>
2024-07-14 23:11:52 +0900

    [feat] 캐릭터 정보 검색 페이지 '최근 검색' 추가

---
+++ src/app/profile/_component/RecentSearch.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { _getLocalStorage } from '@/lib/Util/localStorageUtil'
+import { useEffect, useState } from 'react'
+import styles from './RecentSearch.module.scss'
+
+export default function RecentSearch() {
+  const [recentSearch, setRecentSearch] = useState<string[]>([])
+  // .filter((character: string) => character !== characterName)
+  useEffect(() => {
+    const localStorageData = _getLocalStorage('characterName')
+    console.log('localStorageData', localStorageData)
+
+    if (localStorageData) {
+      setRecentSearch(localStorageData)
+    }
+  }, [])
+
+  return (
+    <section className={styles.RecentSearchContainer}>
+      <h4>최근 검색</h4>
+      {recentSearch && recentSearch.length > 0 && (
+        <ul>
+          {recentSearch.map((character: string, i: number) => (
+            <li key={`recentSearch-${i}`}>{character}</li>
+          ))}
+        </ul>
+      )}
+    </section>
+  )
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,8 +3,16 @@
  * 캐릭터 정보 입력 페이지
  */
 
+import SearchGuide from './_component/SearchGuide'
 import SearchForm from '@/app/profile/_component/SearchForm'
+import RecentSearch from './_component/RecentSearch'
 
 export default function page() {
-  return <SearchForm />
+  return (
+    <>
+      <SearchGuide />
+      <SearchForm />
+      <RecentSearch />
+    </>
+  )
 }
