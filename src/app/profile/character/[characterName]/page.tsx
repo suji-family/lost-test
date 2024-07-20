e34bb9aa588d9bdabc980834bb73fe8a13813600
KwangEun Ahn <61385865+ahnkwang7379@users.noreply.github.com>
2024-07-20 16:15:32 +0900

    [feat] 메타 데이터 추가

--- src/app/profile/character/[characterName]/page.tsx
+++ src/app/profile/character/[characterName]/page.tsx
@@ -1,12 +1,21 @@
+import type { Metadata } from 'next'
 import { Suspense } from 'react'
-import CharacterDetail from './_component/CharacterDetail'
 import Skeleton from 'react-loading-skeleton'
+import CharacterDetail from './_component/CharacterDetail'
 import styles from './page.module.scss'
 
 /**
  * @description
  * 검색된 캐릭터 정보 페이지
  */
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const characterName = decodeURIComponent(params.characterName)
+
+  return {
+    title: `${characterName}의 검색 결과`,
+  }
+}
 
 interface Props {
   params: {
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,14 @@
  * 캐릭터 정보 입력 페이지
  */
 
-import SearchGuide from './_component/SearchGuide'
 import SearchForm from '@/app/profile/_component/SearchForm'
+import { Metadata } from 'next'
 import RecentSearch from './_component/RecentSearch'
+import SearchGuide from './_component/SearchGuide'
+
+export const metadata: Metadata = {
+  title: '전투정보실',
+}
 
 export default function page() {
   return (
