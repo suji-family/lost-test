b5e10f3ec91e67a19cc24bb1a9e260d64b2c4e50
KwangEun Ahn <61385865+ahnkwang7379@users.noreply.github.com>
2024-07-20 19:00:33 +0900

    [refactor] 폴더 구조 변경

--- src/app/_component/mobile/BottomNavigator.tsx
+++ src/app/_component/mobile/BottomNavigator.tsx
@@ -3,8 +3,8 @@
  * 모바일 버전 바텀 네비게이션
  */
 
+import Logo from '../Logo'
 import styles from './BottomNavigator.module.scss'
-import Logo from './Logo'
 
 export default function BottomNavigator() {
   return (
