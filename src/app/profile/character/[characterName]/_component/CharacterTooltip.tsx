ab5c6143f7195abb200c1d71cf3ce8a025b63058
otw03 <86701873+otw03@users.noreply.github.com>
2024-07-22 19:48:52 +0900

    [style] 캐릭터 정보창 툴팁 스타일 추가

--- src/app/profile/character/[characterName]/_component/CharacterTooltip.tsx
+++ src/app/profile/character/[characterName]/_component/CharacterTooltip.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Tooltip } from 'react-tooltip'
+import styles from './CharacterTooltip.module.scss'
 
 interface Props {
   tooltip: string[]
@@ -8,8 +9,6 @@ interface Props {
 }
 
 export default function CharacterTooltip({ tooltip, tooltipId }: Props) {
-  console.log('tooltip', tooltip)
-
   // HTML 태그를 제거하고 텍스트만 추출
   const plainTextTooltips = tooltip
     .map((htmlString) => htmlString.replace(/<[^>]*>/g, ''))
@@ -28,7 +27,7 @@ export default function CharacterTooltip({ tooltip, tooltipId }: Props) {
         html={`<ul>
                         ${plainTextTooltips.map((item) => `<li>${item}</li>`).join('')}
                     </ul>`}
-        style={{ zIndex: 9999 }}
+        className={styles.tooltip}
       />
     </>
   )
